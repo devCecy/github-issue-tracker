94cc1af39d25232e57a81b41ec553e9bdab2cbdb
devCecy <im.ceciliaan@gmail.com>
2023-01-16 17:04:47 +0900

    feat : 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+// components
+import Header from "./components/Header";
+import Bookmark from "./pages/bookmark/Bookmark";
+import IssueBrowse from "./pages/issueBrowse/IssueBrowse";
+
 function App() {
-	return <div></div>;
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<IssueBrowse />} />
+				<Route path="/bookmark" element={<Bookmark />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
