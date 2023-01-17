4165f248bc3a94a4230cd7932a28e4e406aeccd2
devCecy <im.ceciliaan@gmail.com>
2023-01-17 14:52:56 +0900

    feat : 검색 페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Bookmark from "./pages/bookmark/Bookmark";
 import IssueBrowse from "./pages/issueBrowse/IssueBrowse";
+import Search from "./pages/search/Search";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<IssueBrowse />} />
 				<Route path="/bookmark" element={<Bookmark />} />
+				<Route path="/search" element={<Search />} />
 			</Routes>
 		</>
 	);
