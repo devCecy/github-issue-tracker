9033c562fc3af26d233377a016df393e3198cef6
devCecy <im.ceciliaan@gmail.com>
2023-01-17 14:51:51 +0900

    feat : 북마트 삭제 hook

---
+++ src/hooks/useBookmarkDelete.ts
@@ -0,0 +1,26 @@
+import { snackbarState } from "./../atom/snackBarState";
+import { bookmarkArrayState, bookmarkState } from "src/atom/bookmarkState";
+import { useSetRecoilState, useRecoilValue, useRecoilState } from "recoil";
+import { setLocalStorage } from "src/utils/util";
+
+/**
+ * 레포지토리 북마크를 삭제합니다.
+ */
+const useBookmarkDelete = () => {
+	const setBookmarkedByString = useSetRecoilState(bookmarkState);
+	const [snackbar, setSnackbar] = useRecoilState(snackbarState);
+	const bookmarkedArray = useRecoilValue(bookmarkArrayState);
+
+	const handleBookmarkDelete = (targetRepo) => {
+		const newBookmarkList = bookmarkedArray.filter((el) => el !== targetRepo);
+
+		setLocalStorage("bookmarkedRepos", JSON.stringify(newBookmarkList));
+		setBookmarkedByString(JSON.stringify(newBookmarkList));
+
+		setSnackbar({ ...snackbar, isOpen: true, state: "delete" });
+	};
+
+	return handleBookmarkDelete;
+};
+
+export default useBookmarkDelete;
