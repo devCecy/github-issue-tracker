246b8af360b38d1fd433f1ae6041ce8c98267a17
devCecy <im.ceciliaan@gmail.com>
2023-01-18 23:42:48 +0900

    fix : lazy import

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
-import { bookmarkState } from "./atom/bookmarkState";
-import { getLocalStorage } from "./utils/util";
+import { bookmarkState } from "src/atom/bookmarkState";
+import { getLocalStorage } from "src/utils/util";
 
 // components
-import Header from "./components/Header";
-import Bookmark from "./pages/bookmark/Bookmark";
-import IssueBrowse from "./pages/issueBrowse/IssueBrowse";
-import Search from "./pages/search/Search";
+import Header from "src/components/Header";
+const IssueBrowse = lazy(() => import("src/pages/issueBrowse/IssueBrowse"));
+const Bookmark = lazy(() => import("src/pages/bookmark/Bookmark"));
+const Search = lazy(() => import("src/pages//search/Search"));
 
 function App() {
 	const setBookmarkedByString = useSetRecoilState(bookmarkState);
@@ -17,14 +17,17 @@ function App() {
 		setBookmarkedByString(getLocalStorage("bookmarkedRepos"));
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
+
 	return (
 		<>
 			<Header />
-			<Routes>
-				<Route path="/" element={<IssueBrowse />} />
-				<Route path="/bookmark" element={<Bookmark />} />
-				<Route path="/search" element={<Search />} />
-			</Routes>
+			<Suspense fallback={null}>
+				<Routes>
+					<Route path="/" element={<IssueBrowse />} />
+					<Route path="/bookmark" element={<Bookmark />} />
+					<Route path="/search" element={<Search />} />
+				</Routes>
+			</Suspense>
 		</>
 	);
 }
